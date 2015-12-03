8df6ad7ed5a73900274fb1c64f08139a6d3f451d
jbrinkman <joe@dnncorp.com>
2015-12-03 11:08:53 -0500

    Add SPA module files

---
+++ src/DnnSpaModule/Services/ServiceRouteMapper.cs
@@ -0,0 +1,28 @@
+﻿
+using DotNetNuke.Web.Api;
+using System.Web.Http;
+
+namespace Dnn.Modules.DnnSpaModule.Services
+{
+
+    /// <summary>
+    /// The ServiceRouteMapper tells the DNN Web API Framework what routes this module uses
+    /// </summary>
+    public class ServiceRouteMapper : IServiceRouteMapper
+    {
+        /// <summary>
+        /// RegisterRoutes is used to register the module's routes
+        /// </summary>
+        /// <param name="mapRouteManager"></param>
+        public void RegisterRoutes(IMapRoute mapRouteManager)
+        {
+            mapRouteManager.MapHttpRoute(
+                moduleFolderName:"DnnSpaModule",
+                routeName: "default", 
+                url: "{controller}/{itemId}", 
+                defaults: new { itemId = RouteParameter.Optional }, 
+                namespaces: new[] { "Dnn.Modules.DnnSpaModule.Services" });
+        }
+    }
+
+}
---
+++ src/DnnSpaModule/Services/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Collections.Generic;
+using Dnn.Modules.DnnSpaModule.Services.ViewModels;
+using DotNetNuke.Web.Api;
+using DotNetNuke.Security;
+using DotNetNuke.Entities.Users;
+
+namespace Dnn.Modules.DnnSpaModule.Services
+{
+    [SupportedModules("DnnSpaModule")]
+    [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Edit)]
+    public class UserController : DnnApiController
+    {
+        public UserController() { }
+
+        public HttpResponseMessage GetList()
+        {
+
+            var userlist = DotNetNuke.Entities.Users.UserController.GetUsers(this.PortalSettings.PortalId);
+            var users = userlist.Cast<UserInfo>().ToList()
+                   .Select(user => new UserViewModel(user))
+                   .ToList();
+
+            return Request.CreateResponse(users);
+        }
+    }
+}
