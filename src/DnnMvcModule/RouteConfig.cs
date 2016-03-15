72e2d20485fc67693b77e4a36a5393d3a4c1ad99
Amarjit Dhunna <amarjit.dhunna@dnnsoftware.com>
2016-03-15 12:13:44 -0700

    Updated references. Added RouteConfig.cs Created New VSIX template files.

---
+++ src/DnnMvcModule/RouteConfig.cs
@@ -0,0 +1,24 @@
+﻿/*
+' Copyright (c) $year$ $ownername$
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+using DotNetNuke.Web.Mvc.Routing;
+
+namespace Dnn.Modules.DnnMvcModule
+{
+    public class RouteConfig : IMvcRouteMapper
+    {
+        public void RegisterRoutes(IMapRoute mapRouteManager)
+        {
+            mapRouteManager.MapRoute("Dnn.Modules.DnnMvcModule", "Dnn.Modules.DnnMvcModule", "{controller}/{action}", new[]
+            {"Dnn.Modules.DnnMvcModule.Controllers"});
+        }
+    }
+}
